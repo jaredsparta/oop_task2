c76537b1baf739f11ee9f49f44261f0a4427a74f
jared <jtlss14@gmail.com>
2020-11-05 15:59:19 +0000

    finished pseudo coding

--- menu.py
+++ menu.py
@@ -5,13 +5,13 @@ class Menu(Item):
         self.menu = []
         self.dictionary = {}
 
-    
+    # Adds an item to the menu
     def add_item(self, name, price):
         item_to_add = Item(name, price)
         self.menu.append(item_to_add)
         self.dictionary[item_to_add.name] = item_to_add
 
-
+    # Lists the items on the menu
     def list_menu(self):
         for item in self.menu:
             print(item)
