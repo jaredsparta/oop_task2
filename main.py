3b64514a1659453ca8884d81318e8e164f154f01
jared <jtlss14@gmail.com>
2020-11-05 15:58:36 +0000

    pseudo code

--- main.py
+++ main.py
@@ -14,18 +14,20 @@ Jared.menu.add_item("Salad", 7)
 
 print(f"Hi there, I'm {Jared.name}. Can I take your order? ")
 
+# This code will run until we insert 4 as a choice
 while True:
     choice = input("""
-                    1. I can show you our menu or
-                    2. You can order something or
-                    3. I can tell you your order
-                    4. Or are you finished with ordering?
+                    1. Show menu
+                    2. Order something
+                    3. Show current order list
+                    4. Finish with ordering
                      ->  """)
 
+    # Lists the menu
     if choice == "1":
         Jared.list_items()
 
-    
+    # Allows you to order
     if choice == "2":
         order = input("\nChoose an item on the menu please: ")
         if order.title() in Jared.menu.dictionary:
@@ -34,11 +36,12 @@ while True:
             print("Sorry, we don't have that")
             continue
     
-
+    # Checks the orders you've mad
     if choice == "3":
+        print("\nYou have ordered the following: ")
         Jared.check_orders()
 
-
+    # Exits the while loop and program finishes
     if choice == "4":
         break
     
--- waiter.py
+++ waiter.py
@@ -7,16 +7,16 @@ class Waiter(Menu):
         self.menu = menu
         self.orders = []
 
+    # Lists the items in the menu
     def list_items(self):
         print("\nWe are currently offering the following items:")
         self.menu.list_menu()
 
-
+    # Adds the order to the list
     def add_order(self, item):
         self.orders.append(item)
         
-        
-
+    # Gives the list of orders
     def check_orders(self):
         for order in self.orders:
             print(order)
